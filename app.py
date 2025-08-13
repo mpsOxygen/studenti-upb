4f14298ff6c994114c79bfe5d824a12b5926785a
Razvan Valceanu <razvan.valceanu@metaminds.com>
2025-08-13 16:20:47 +0300

    fix linter errors
    Signed-off-by: Razvan Valceanu <razvan.valceanu@metaminds.com>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def application(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/plain; charset=utf-8')]
@@ -7,5 +8,3 @@ def application(environ, start_response):
     response_body = f'{current_time} Hello, studenti UPB!\n'.encode('utf-8')
     start_response(status, headers)
     return [response_body]
-
-
